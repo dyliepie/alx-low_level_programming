efadc7c99d8aa31f327940fd312146b6d3bd64f0
dyliepie <dylansarlie01@gmail.com>
2023-06-20 14:11:13 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,10 +5,10 @@
  * @n: The number to be checked
  *
  * Description:
- *   This function checks the sign of the number 'n' and prints the corresponding
- *   sign symbol. If 'n' is greater than zero, it returns 1 and prints '+'.
- *   If 'n' is zero, it returns 0 and prints '0'. If 'n' is less than zero,
- *   it returns -1 and prints '-'.
+ *   This function checks the sign of the number 'n' and prints the
+ *   corresponding sign symbol. If 'n' is greater than zero, it returns 1
+ *   and prints '+'. If 'n' is zero, it returns 0 and prints '0'. If 'n'
+ *   is less than zero, it returns -1 and prints '-'.
  *
  * Return: 1 if 'n' is greater than zero, 0 if 'n' is zero,
  *         -1 if 'n' is less than zero
