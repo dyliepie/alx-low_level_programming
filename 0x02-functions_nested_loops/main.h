ee875c1b34e15592144358880fd4625c0beef451
dyliepie <dylansarlie01@gmail.com>
2023-06-20 06:16:47 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _is_lower(int c);
+int _is_alpha(int c);
+int print_sign(int n);
+int abs(int n);
+int print_last_digit(int n);
+void jack_bauer(void);
+void times_table(void);
+int add(int a, int b);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif
