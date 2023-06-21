d963ef93f63b42ad6f5a7ca4b414ac42d0acdecd
dyliepie <dylansarlie01@gmail.com>
2023-06-21 05:55:28 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -23,20 +23,20 @@ int main(void)
 		fib2 = sum;
 	}
 
-	fib1_half1 = fib1 / 10000000000UL;
-	fib1_half2 = fib1 % 10000000000UL;
-	fib2_half1 = fib2 / 10000000000UL;
-	fib2_half2 = fib2 % 10000000000UL;
+	fib1_half1 = fib1 / 10000000000;
+	fib1_half2 = fib1 % 10000000000;
+	fib2_half1 = fib2 / 10000000000;
+	fib2_half2 = fib2 % 10000000000;
 
 	for (count = 92; count < 98; count++)
 	{
 		half1 = fib1_half1 + fib2_half1;
 		half2 = fib1_half2 + fib2_half2;
 
-		if (fib1_half2 + fib2_half2 > 9999999999UL)
+		if (fib1_half2 + fib2_half2 > 9999999999)
 		{
 			half1 += 1;
-			half2 %= 10000000000UL;
+			half2 %= 10000000000;
 		}
 
 		printf(", %lu%010lu", half1, half2);
