48170644a051831855193b646a24ffe0799600b7
dyliepie <dylansarlie01@gmail.com>
2023-06-21 01:59:41 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#define LIMIT 1024
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < LIMIT; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return 0;
+}
